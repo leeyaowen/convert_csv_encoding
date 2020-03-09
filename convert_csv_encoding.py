47162fde0e28096c4d3abdbd472616cfe5a7ead7
leeyaowen <jcjh30101@gmail.com>
2020-03-09 16:52:55 +0800

    批次轉換編碼

--- convert_csv_encoding.py
+++ convert_csv_encoding.py
@@ -1,5 +1,25 @@
 import glob
 
-for filename in glob.glob('*.csv'):
-    s = open(filename, mode='r', encoding='utf8').read()
-    open(filename, mode='w', encoding='ANSI').write(s)
+while True:
+    # noinspection PyBroadException
+    try:
+        input_encode = input('input_encode=? ')
+        output_encode = input('output_encode=? ')
+        for filename in glob.glob('*.csv'):
+            with open(filename, mode='r', encoding=str(input_encode)) as input_file:
+                content = input_file.read()
+            with open(filename, mode='w', encoding=str(output_encode)) as output_file:
+                output_file.write(content)
+        print('Successfully convert!')
+    except Exception:
+        print('There are some errors!!')
+
+    # noinspection PyBroadException
+    try:
+        ans = input('\nWant to close? ')
+        if str(ans) == 'yes':
+            break
+        else:
+            continue
+    except Exception:
+        continue
